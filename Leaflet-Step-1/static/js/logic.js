8e948d7c3db3e4a7b21eed4682314ca4bd656b6b
Duncan Plate <duncanplate@gmail.com>
2021-09-06 11:22:45 -0500

    it SHOULD be working

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,8 +1,37 @@
 base_url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 
-let colors = ["red", "orange", "pink", "yellow", "blue", "green"];
+function circleColor(depth) {
+    if (depth <= 10) {
+        return "#D1FF33";
+    } else if (depth <= 30) {
+        return "#EAFF33";
+    } else if (depth <= 50) {
+        return "#FFF533";
+    } else if (depth <= 70) {
+        return "#FFCD33";
+    } else if (depth <= 90) {
+        return "#FF8F33";
+    } else {
+        return "#FF3633";
+    };
+};
 
+function circleRadius(mag) {
+    if (mag <= 1) {
+        return 10000;
+    } else if (mag <= 2){
+        return 25000;
+    } else if (mag <= 3){
+        return 38000;
+    } else if (mag <= 4){
+        return 50000;
+    } else if (mag <= 5){
+        return 6000;
+    } else {
+        return 80000;
+    }
+};
 
 d3.json(base_url).then(function (data) {
 
@@ -11,18 +40,30 @@ d3.json(base_url).then(function (data) {
 
 function createFeatures(earthquakeData) {
 
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
-  }
-
-
   var earthquakes = L.geoJSON(earthquakeData, {
     onEachFeature: onEachFeature
   });
 
+
   
+function onEachFeature(feature, layer) {
+    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p><br>
+                    Magnitude: ${feature.properties.mag}<br>
+                    ${new Date(feature.properties.time)}<br>
+                    Depth: ${feature.geometry.coordinates[2]} km</p>`),
+  
+      pointToLayer: function (feature, location) {
+        return new L.circle(location, {
+        fillColor: circleColor(feature.geometry.coordinates[2]),
+        radius:  circleRadius(feature.properties.mag),
+        fillOpacity: .75,
+        stroke: true,
+        weight: 1
+   })
+
+ };
   createMap(earthquakes);
-}
+}};
 
  
 function createMap(earthquakes) {
@@ -50,17 +91,26 @@ function createMap(earthquakes) {
   // Create our map, giving it the streetmap and earthquakes layers to display on load.
   var myMap = L.map("map", {
     center: [
-      37.09, -95.71
+      42.09, -92.78
     ],
-    zoom: 3,
-    layers: [street, earthquakes]
+    zoom: 4,
+    layers: [street, topo, earthquakes]
   });
 
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+  
 
 }
+var legend = L.control({position: 'topright'});
+
+legend.onAdd = function (map) {
+   var div = L.DomUtil.create('div', 'info legend'),
+   depths =  [0, 25, 50, 75, 100],
+   labels = [];
+   for (var i = 0; i < depths.length; i++) {
+     div.innerHtml +=
+      `<i style="backround:' + circleColor(depths[i] + 1) + '"></i> ` 
+      depths[i] + depths[i] + 1 ? '&ndash;' + depths[i +1] + '<br>' : '+');
+   };
+   return  div;
+};
+legend.addTo(myMap);
